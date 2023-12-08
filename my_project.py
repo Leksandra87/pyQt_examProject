9c056fc57fdd48b31a0c50eceb9d87bdc837d778
Leksandra87 <petroselinuma@gmail.com>
2023-12-08 20:04:59 +0300

    done

--- my_project.py
+++ my_project.py
@@ -1,6 +1,34 @@
 from PySide6 import QtWidgets, QtCore
+import time
 import psutil
 import platform
+
+
+class SystemInfo(QtCore.QThread):
+    """
+    поток для получения системной информации
+
+    """
+    systemInfoReceived = QtCore.Signal(list)
+
+    def __init__(self, timeout=1, parent=None):
+        super().__init__(parent)
+        self.timeout = timeout
+        self.status = True
+
+    def run(self) -> None:
+        self.status = True
+
+        while self.status:
+            sys_info = []
+            print(psutil.cpu_percent())
+            sys_info.append(psutil.cpu_percent())  # загрузка процессора
+            # print(psutil.virtual_memory().percent)
+            sys_info.append(psutil.virtual_memory().percent)  # загрузка оперативной памяти
+
+            self.systemInfoReceived.emit(sys_info)
+
+            time.sleep(self.timeout)
 
 
 class Window(QtWidgets.QWidget):
@@ -8,7 +36,9 @@ class Window(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.initThreads()
         self.initUi()
+        self.initSignals()
 
     def initUi(self) -> None:
         """
@@ -16,6 +46,8 @@ class Window(QtWidgets.QWidget):
 
         :return: None
         """
+        self.setWindowTitle('Мониторинг системы')
+
         lableProcessorName = QtWidgets.QLabel("Процессор")
         lableProcessorName.setMinimumWidth(120)
         self.ProcNameLineEdit = QtWidgets.QLineEdit()
@@ -51,7 +83,7 @@ class Window(QtWidgets.QWidget):
         GBprocessor = QtWidgets.QGroupBox("Процессор")
         GBprocessor.setLayout(layoutProc)
 
-# ----------------------------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------------------------
         lableRAMtotal = QtWidgets.QLabel('Объём памяти')
         lableRAMtotal.setMinimumWidth(120)
         self.RAMtotalLineEdit = QtWidgets.QLineEdit()
@@ -75,10 +107,10 @@ class Window(QtWidgets.QWidget):
         layoutRAM.addLayout(layoutRAMcurret)
         layoutRAM.addWidget(self.RAMload)
 
-        GBram = QtWidgets.QGroupBox("...")
+        GBram = QtWidgets.QGroupBox("Оперативная память")
         GBram.setLayout(layoutRAM)
 
-# ----------------------------------------------------------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------------------------
         lableDisk = QtWidgets.QLabel("Количество жестких дисков")
         lableDisk.setMinimumWidth(160)
         self.DiskLineEdit = QtWidgets.QLineEdit()
@@ -91,47 +123,83 @@ class Window(QtWidgets.QWidget):
         layoutDisk.addLayout(layoutD)
         layoutDisk.addWidget(self.DiskInfoPlaintext)
 
-        GBdisk = QtWidgets.QGroupBox("...")
+        GBdisk = QtWidgets.QGroupBox("Жесткий диск")
         GBdisk.setLayout(layoutDisk)
-
-
 
         FirstTabLayout = QtWidgets.QVBoxLayout()
         FirstTabLayout.addWidget(GBprocessor)
         FirstTabLayout.addWidget(GBram)
         FirstTabLayout.addWidget(GBdisk)
 
+        frame = QtWidgets.QFrame()
+        frame.setLayout(FirstTabLayout)
 
+        # ----------------------------------------------------------------------------------------------------------------------
 
+        self.proccesPlaintextEdit = QtWidgets.QPlainTextEdit()
+        self.proccesPlaintextEdit.setReadOnly(True)
+        self.servisePlaintextedit = QtWidgets.QPlainTextEdit()
+        self.servisePlaintextedit.setReadOnly(True)
+        self.tasksPlaintextEdit = QtWidgets.QPlainTextEdit()
+        self.tasksPlaintextEdit.setReadOnly(True)
 
-        # self.systemPlaintextEdit = QtWidgets.QPlainTextEdit()
-        # self.systemPlaintextEdit.setReadOnly(True)
-        # self.proccesPlaintextEdit = QtWidgets.QPlainTextEdit()
-        # self.proccesPlaintextEdit.setReadOnly(True)
-        # self.servisePlaintextedit = QtWidgets.QPlainTextEdit()
-        # self.servisePlaintextedit.setReadOnly(True)
-        # self.tasksPlaintextEdit = QtWidgets.QPlainTextEdit()
+        self.tabWidget = QtWidgets.QTabWidget()
+        self.tabWidget.addTab(frame, 'Система')
+        self.tabWidget.addTab(self.proccesPlaintextEdit, 'Процессы')
+        self.tabWidget.addTab(self.servisePlaintextedit, 'Службы')
+        self.tabWidget.addTab(self.tasksPlaintextEdit, 'Задачи')
 
-        # self.tabWidget = QtWidgets.QTabWidget()
-        # self.tabWidget.addTab(self.systemPlaintextEdit, 'Система')
-        # self.tabWidget.addTab(self.proccesPlaintextEdit, 'Процессы')
-        # self.tabWidget.addTab(self.servisePlaintextedit, 'Службы')
-        # self.tabWidget.addTab(self.tasksPlaintextEdit, 'Задачи')
-        #
-        # self.spinBoxLabel = QtWidgets.QLabel("Чатсота обновления")
-        # self.spinBox = QtWidgets.QSpinBox()
-        # self.spinBox.setRange(1, 30)
-        #
-        # spinboxLayout = QtWidgets.QHBoxLayout()
-        # spinboxLayout.addWidget(self.spinBoxLabel)
-        # spinboxLayout.addWidget(self.spinBox)
-        #
-        # mainLayout = QtWidgets.QVBoxLayout()
-        # mainLayout.addWidget(self.tabWidget)
-        # mainLayout.addLayout(spinboxLayout)
-        #
-        self.setLayout(FirstTabLayout)
+        ComboBoxLabel = QtWidgets.QLabel("Чатсота обновления, сек.")
+        self.ComboBox = QtWidgets.QComboBox()
+        self.ComboBox.addItems(['1', '5', '10', '30'])
+
+        comboboxLayout = QtWidgets.QHBoxLayout()
+        comboboxLayout.addWidget(ComboBoxLabel)
+        comboboxLayout.addWidget(self.ComboBox)
+
+        mainLayout = QtWidgets.QVBoxLayout()
+        mainLayout.addWidget(self.tabWidget)
+        mainLayout.addLayout(comboboxLayout)
+
+        self.setLayout(mainLayout)
         # self.setMinimumSize(500, 400)
+
+    def initSignals(self) -> None:
+        """
+        Инициализация сигналов
+
+        """
+
+        self.ComboBox.currentTextChanged.connect(self.setTimeoutForSysInfo(int(self.ComboBox.currentText())))
+        self.ProcNameLineEdit.setText(platform.uname().processor)
+        self.CoresLineEdit.setText(str(psutil.cpu_count(logical=True)))
+        self.systemInfo.systemInfoReceived.connect(self.setProcInfo)
+
+    def setProcInfo(self, data):
+        self.ProcessorLoadLineEdit.setText(str(data[0]))
+        self.ProcessorLoadPB.setValue(data[0])
+
+
+
+
+    def initThreads(self) -> None:
+        """
+        Инициализация потока
+
+        :return:
+        """
+        self.systemInfo = SystemInfo()
+        self.systemInfo.start()
+
+    def setTimeoutForSysInfo(self, value):
+        """
+        Установка частоты обновления информации в потоке
+
+        """
+
+        self.systemInfo.timeout = value
+
+
 
 
 if __name__ == "__main__":
